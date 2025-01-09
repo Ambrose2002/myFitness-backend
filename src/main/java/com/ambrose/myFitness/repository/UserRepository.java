4f128e656c773a00220c52ac6ae6b3dddf3fb938
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-08 23:21:36 -0500

    create user repository

---
+++ src/main/java/com/ambrose/myFitness/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ambrose.myFitness.repository;
+
+import com.ambrose.myFitness.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
