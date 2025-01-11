40c59d619624e0329627c053dde9d7871b984129
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-10 22:21:35 -0500

    add hashing to createuser

--- src/main/java/com/ambrose/myFitness/service/UserService.java
+++ src/main/java/com/ambrose/myFitness/service/UserService.java
@@ -4,6 +4,7 @@ import com.ambrose.myFitness.model.User;
 import com.ambrose.myFitness.repository.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -21,6 +25,7 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
