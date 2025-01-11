7118515bcf8294cb6a76de650169c7e3ff66fa5e
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-10 22:36:55 -0500

    create jwt util

---
+++ src/main/java/com/ambrose/myFitness/Utilities/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.ambrose.myFitness.Utilities;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "secret";
+    private final long EXPIRATION_TIME = 86400000; // 1 day in milliseconds
+
+    // Generate Token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    // Validate Token
+    public boolean validateToken(String token, String username) {
+        String tokenUsername = extractUsername(token);
+        return username.equals(tokenUsername) && !isTokenExpired(token);
+    }
+
+    // Extract Username
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Check Expiration
+    public boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+}
---
+++ src/main/java/com/ambrose/myFitness/controller/AuthenticationController.java
@@ -0,0 +1,2 @@
+package com.ambrose.myFitness.controller;public class AuthenticationController {
+}
