ec6eb7e437fe63fc7c22cae588eb6f6a7e317caa
Ambrose Blay <ambrose2002blay@gmail.com>
2025-01-10 22:52:44 -0500

    create and implement authrequest dto

--- src/main/java/com/ambrose/myFitness/dto/AuthRequest.java
+++ src/main/java/com/ambrose/myFitness/dto/AuthRequest.java
@@ -1,2 +1,22 @@
-package com.ambrose.myFitness.dto;public class AuthRequest {
+package com.ambrose.myFitness.dto;
+
+public class AuthRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
